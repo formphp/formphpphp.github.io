dd176a3ee680bfce9f8d64e93280d8eb6ca3be1a
formphp <109471599+formphp@users.noreply.github.com>
2022-07-17 21:20:24 +0545

    Add files via upload

---
+++ basefontsize.c
@@ -0,0 +1,16 @@
+# include<stdio.h>
+
+int main(){
+    int year,remainder;
+    printf("year   ");
+    scanf("%d",&year);
+    if(remainder=)
+    {
+        printf("this is leap year %d \n ",year);
+    }
+    else
+    {
+        printf("not a leap year %d \n",year); 
+    }
+    return 0;
+}
